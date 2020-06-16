8c1e8395ac4fe45024887b183ed417c7b0fa65f2
ChetanSagathiya <chetuu.018@gmail.com>
2020-06-16 16:24:27 +0530

    changes in setting.py

--- karma/settings.py
+++ karma/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5m7aem0$h-a2)ip-2-t&vr!%d&qo6e$0)st4jxj!sa@itp&1dg'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
